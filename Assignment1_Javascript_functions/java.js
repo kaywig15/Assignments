423121e7e7d1407ce5e5fa4f898cb86c2cd877c0
Kyle Orwig <kyleorwig@gmail.com>
2024-10-09 16:49:33 -0700

    updated solution

--- Assignment1_Javascript_functions/java.js
+++ Assignment1_Javascript_functions/java.js
@@ -1,2 +1,23 @@
-let number1 = parseFloat(document.getElementById("number1").value);
-let number2 = parseFloat(document.getElementById("number2").value);
+function handleAddition() {
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+  document.getElementById("result").value = number1 + number2;
+}
+
+function handleSubtraction() {
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+  document.getElementById("result").value = number1 - number2;
+}
+
+function handleMultiplaction() {
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+  document.getElementById("result").value = number1 * number2;
+}
+
+function handleDivision() {
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+  document.getElementById("result").value = number1 / number2;
+}
