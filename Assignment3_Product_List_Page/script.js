e441ca9a8459fff363d3472517a0ff797ba22dda
Kyle Orwig <kyleorwig@gmail.com>
2024-10-18 09:38:04 -0700

    begin working on function

--- Assignment3_Product_List_Page/script.js
+++ Assignment3_Product_List_Page/script.js
@@ -1,4 +1,4 @@
-let product1 = {
+const product1 = {
   dogBreed: "Corgi",
   price: 1500,
   description: "High energy small herding dog",
@@ -6,14 +6,8 @@ let product1 = {
     "https://www.dailypaws.com/thmb/hr2f0QpU6Bywd8iWj44wGmkfltM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pembroke-welsh-corgi-on-stairs-1163785112-2000-142d1a9393dd450d9ef59cd6b65ce15c.jpg",
 };
 
-let products = [
-  {
-    dogBreed: "Corgi",
-    price: 1500,
-    description: "High energy small herding dog",
-    image:
-      "https://www.dailypaws.com/thmb/hr2f0QpU6Bywd8iWj44wGmkfltM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pembroke-welsh-corgi-on-stairs-1163785112-2000-142d1a9393dd450d9ef59cd6b65ce15c.jpg",
-  },
+const products = [
+  product1,
   {
     dogBreed: "Labrador",
     price: 2000,
@@ -22,10 +16,14 @@ let products = [
       "https://myredfoxlabs.com/wp-content/uploads/ginger-red-lab-female-768x960.webp",
   },
   {
-    dogBreed: "Rotweiler",
+    dogBreed: "Rottweiler",
     price: 1000,
     description: "Large dog that is great with kids",
     image:
       "https://www.lovemydogz.com/wp-content/smush-webp/2021/04/rottweiler-kocka-scaled.jpg.webp",
   },
 ];
+function displayProducts(products) {
+  const container = document.createElement("div");
+  container.className = "product-container";
+}
