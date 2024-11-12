f2734a6f0be27128be4f127b498fa5503242e33c
Kyle Orwig <kyleorwig@gmail.com>
2024-11-12 10:05:17 -0800

    adding assignment 4

---
+++ Assignment4_JavaScript_OOP/script.js
@@ -0,0 +1,9 @@
+class Book {
+  constructor() {
+    (this.title = "The Demon of Unrest"),
+      (this.author = "Erik Larson"),
+      (this.genre = "Nonfiction"),
+      (this.year = "2024"),
+      console.log("Class constructor is run");
+  }
+}
