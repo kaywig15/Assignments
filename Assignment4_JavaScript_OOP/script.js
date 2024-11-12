4c1aa96724a69bb6cb4df3d5e87a41ae102258ab
Kyle Orwig <kyleorwig@gmail.com>
2024-11-12 10:32:38 -0800

    updated assignment 4

--- Assignment4_JavaScript_OOP/script.js
+++ Assignment4_JavaScript_OOP/script.js
@@ -1,9 +1,43 @@
 class Book {
-  constructor() {
-    (this.title = "The Demon of Unrest"),
-      (this.author = "Erik Larson"),
-      (this.genre = "Nonfiction"),
-      (this.year = "2024"),
-      console.log("Class constructor is run");
+  constructor(title, author, genre, year) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.year = year;
+  }
+  displayDetails() {
+    console.log(`Title: ${this.title}`);
+    console.log(`Author: ${this.author}`);
+    console.log(`Genre: ${this.genre}`);
+    console.log(`Year: ${this.year}`);
+  }
+
+  getSummary() {
+    return `Title: ${this.title}, Author: ${this.author}, Genre: ${this.genre}, Year: ${this.year}`;
   }
 }
+
+const book1 = new Book(
+  "The Demon of Unrest",
+  "Erik Larson",
+  "Nonfiction",
+  "2024"
+);
+const book2 = new Book(
+  "Brave New World",
+  "Aldous Huxley",
+  "Science Fiction",
+  "1932"
+);
+const book3 = new Book(
+  "Nineteen Eighty-Four",
+  "George Orwell",
+  "Dystopian",
+  "1949"
+);
+
+const books = [book1, book2, book3];
+
+books.forEach((book) => {
+  console.log(book.getSummary());
+});
